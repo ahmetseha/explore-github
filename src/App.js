28087b84118caa401e8607edb8e931df1d874b85
ahmetseha <sehabi.com@gmail.com>
2021-11-06 13:37:19 +0300

    Add localization to locals

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import { IntlProvider } from "react-intl";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Switch, Route, HashRouter as Router } from "react-router-dom";
 
 import { Provider as LanguageProvider } from "./context/language";
@@ -13,8 +13,13 @@ import Trends from "./pages/Trends";
 import NotFoundPage from "./pages/NotFoundPage";
 
 const App = () => {
-  const defaultLang = navigator.language;
+  const isLocale = localStorage.getItem("locale");
+  const defaultLang = isLocale ? isLocale : navigator.language;
   const [locale, setLocale] = useState(defaultLang);
+
+  useEffect(() => {
+    localStorage.setItem("locale", locale);
+  }, [locale]);
 
   return (
     <IntlProvider
