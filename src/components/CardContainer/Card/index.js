5c20530a9411e4159bb46dab8f7c96e5d430e372
ahmetseha <sehabi.com@gmail.com>
2021-11-07 12:36:44 +0300

    Show github repos in explore

--- src/components/CardContainer/Card/index.js
+++ src/components/CardContainer/Card/index.js
@@ -1,7 +1,27 @@
 import styled from "styled-components";
 
-const Card = () => {
-  return <div>Card</div>;
+const CardSection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  border: 1px solid #353d40;
+  grid-gap: 7px;
+  h3 {
+    color: #003f63;
+  }
+`;
+
+const Card = ({ repoName, ahref, repoLang }) => {
+  return (
+    <CardSection>
+      <h3>{repoName}</h3>
+      <p>{repoLang}</p>
+      <a href={ahref} target="_blank">
+        asd
+      </a>
+    </CardSection>
+  );
 };
 
 export default Card;
--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -1,12 +1,80 @@
 import styled from "styled-components";
+import { useState } from "react";
 
 import Card from "./Card";
+import getUserRepos from "../../api";
+
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+const FormSection = styled.form`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin: 30px;
+
+  input {
+    border: 1px solid #353d40;
+    border-radius: 8px;
+    padding: 10px 10px 10px 25px;
+    width: 40%;
+    font-size: 18px;
+    font-weight: 500;
+    letter-spacing: 1px;
+    color: #003f63;
+    &:focus {
+      outline: none;
+    }
+  }
+`;
+const CardContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  grid-gap: 40px;
+`;
 
 const CardContainer = () => {
+  const [name, setName] = useState("");
+  const [repos, setRepos] = useState([]);
+
+  const getUserRepo = (e) => {
+    e.preventDefault();
+    getUserRepos
+      .get(`${name}/repos?sort=created&direction=desc&per_page=300`)
+      .then((res) => {
+        console.log(res.data);
+        setRepos(res.data);
+      });
+  };
   return (
-    <div>
-      <Card />
-    </div>
+    <Container>
+      <FormSection onSubmit={getUserRepo}>
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+      </FormSection>
+      <div>
+        <CardContent>
+          {repos
+            ? repos.map((repo, id) => {
+                return (
+                  <Card
+                    key={id}
+                    repoId={repo.id}
+                    repoName={repo.name}
+                    ahref={repo.html_url}
+                    repoLang={repo.language}
+                  />
+                );
+              })
+            : null}
+        </CardContent>
+      </div>
+    </Container>
   );
 };
 
